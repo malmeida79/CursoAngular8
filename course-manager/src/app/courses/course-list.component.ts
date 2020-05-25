0e9bdd221f7a9745d57ab1bf83a8bc31716cf9bc
mdalmeida79 <marcos.almeida@taking.com.br>
2020-05-25 19:05:08 -0300

    altrerando para usar ao inves do array de cursos para backend e funcao de salvar com observables

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -17,8 +17,27 @@ export class CourseListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.courses = this.courseService.retieveAll();
-        this.filteredCourses = this.courses;
+        this.retrieveAll();
+    }
+
+    retrieveAll(): void {
+        this.courseService.retieveAll().subscribe({
+            // ao executar um sobsribe e obter sucesso sera retornado um next
+            // e nesse receberei entao a lista consultada
+            next: courses => {
+                this.courses = courses;
+                // essa linha foi movida para ca pq como a execução e assicrona somente nesse ponto
+                // consigo ter certeza de que o objeto ja foi retornado. Onde estava antes do lado de
+                // fora do observable eu poderia seguir com a execução do código sem ter de fato a
+                // resposta.
+                this.filteredCourses = this.courses;
+            },
+            error: err => {
+                // caso tenhamos algum erro no observable passará por aqui
+                console.log('Error', err);
+            }
+        });
+
     }
 
     set filter(value: string) {
--- course-manager/src/app/courses/course.service.ts
+++ course-manager/src/app/courses/course.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Course } from './course';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CourseService {
 
-    retieveAll(): Course[] {
-        return COURSES;
+    private courseUrl = 'http://localhost:3100/api/courses';
+
+    constructor(private httpClient: HttpClient) {
+
     }
 
-    retrieveById(id: number): Course {
-        return COURSES.find((courseInteractor: Course) => courseInteractor.id === id);
+    retieveAll(): Observable<Course[]> {
+        return this.httpClient.get<Course[]>(this.courseUrl);
     }
 
-    save(course: Course): void {
+    retrieveById(id: number): Observable<Course> {
+        return this.httpClient.get<Course>(`${this.courseUrl}/${id}`);
+    }
+
+    save(course: Course): Observable<Course> {
         if (course.id) {
-            const index = COURSES.findIndex((courseInteractor: Course) => courseInteractor.id === course.id);
-            COURSES[index] = course;
+            return this.httpClient.put<Course>(`${this.courseUrl}/${course.id}`, course);
+        } else {
+            return this.httpClient.post<Course>(`${this.courseUrl}`, course);
         }
     }
 }
