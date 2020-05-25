a69a7330af8cf6f0f4e5026c90c59cc993989c09
mdalmeida79 <marcos.almeida@taking.com.br>
2020-05-25 20:14:22 -0300

    ajuste da pagina tratamento de erros 404, movida para core

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,29 +4,22 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
-import { core } from '@angular/compiler';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    CoreModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         // rota inicial para base path (vazio)
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        // quando não encontrar a url
-        path: '**', component: Error404Component
       }
     ])
   ],
--- course-manager/src/app/core/core.module.ts
+++ course-manager/src/app/core/core.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { Error404Component } from './component/error-404/error-404.component';
 
 @NgModule({
     declarations: [
-        NavBarComponent
+        NavBarComponent,
+        Error404Component
     ],
-    imports:[
-        RouterModule
+    imports: [
+        RouterModule.forChild([
+            {
+                // quando não encontrar a url
+                path: '**', component: Error404Component
+            }
+        ])
     ],
     exports: [
         NavBarComponent
