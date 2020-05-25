515924cb78aaa4dae48c612e7a95a4305c00064d
mdalmeida79 <marcos.almeida@taking.com.br>
2020-05-25 19:53:16 -0300

    mais segregacao e responsabilidades

--- course-manager/src/app/courses/course.module.ts
+++ course-manager/src/app/courses/course.module.ts
@@ -3,20 +3,20 @@ import { CourseListComponent } from './course-list.component';
 import { CourseInfoComponent } from './course-info.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ReplacePipe } from '../pipe/replace.pipe';
 import { CommonModule } from '@angular/common';
-import { StarComponent } from '../star/star.component';
+import { StarModule } from '../shared/components/star/star.module';
+import { AppPipeModule } from '../shared/pipe/app-pipe.module';
 
 @NgModule({
     declarations: [
         CourseListComponent,
-        CourseInfoComponent,
-        StarComponent,
-        ReplacePipe,
+        CourseInfoComponent
     ],
     imports: [
         FormsModule,
         CommonModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses', component: CourseListComponent
